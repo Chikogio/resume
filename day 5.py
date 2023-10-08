74b34149d0bb4abb17675bc27cf9353e0a8b306b
Chikogio <143025526+Chikogio@users.noreply.github.com>
2023-10-08 12:36:17 +0400

    Add files via upload

---
+++ day 5.py
@@ -0,0 +1,8 @@
+user_salary = float(input("enter your salary: "))
+
+if user_salary>10000:
+    print("გოა-ში სწავლობდი?")
+elif user_salary>1000 and user_salary < 10000:
+    print("you are mid")
+elif user_salary<1000:
+    print("შემოსულიყავი გოაში, მატრიცელო")
